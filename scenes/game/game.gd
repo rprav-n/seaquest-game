adc77f211549339831d413ec0e6bdf8d1076fef9
Praveen <rpraveen.engineer@gmail.com>
2023-11-11 23:01:51 +0530

    Add some mesh + dummy player movement

--- scenes/game/game.gd
+++ scenes/game/game.gd
@@ -21,9 +21,3 @@ func reset_globals() -> void:
 	Global.score = 0
 
 
-
-func _on_screenshot_timer_timeout() -> void:
-	var img = get_viewport().get_texture().get_image()
-	var timestamp: int = int(Time.get_unix_time_from_system())
-
-	img.save_png("res://build/screenshots/screenshot_" + str(timestamp) + ".png")
